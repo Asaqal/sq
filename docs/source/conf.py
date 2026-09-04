8e91dff5b1674eabc0d1175d08950e1eb0195187
Asaqal <saqalahmad@gmail.com>
2026-09-04 04:35:30 -0400

    commit workflows (lint + docs)

---
+++ docs/source/conf.py
@@ -0,0 +1,10 @@
+project = "sq"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+html_theme = "sphinx_rtd_theme"
---
+++ src/sq/__init__.py
@@ -0,0 +1,19 @@
+"""sq (saqal hq (hep-queue))."""
+
+
+def add(a: int, b: int) -> int:
+    """Add two integers.
+
+    Parameters
+    ----------
+    a : int
+        The first integer to add.
+    b : int
+        The second integer to add.
+
+    Returns
+    -------
+    int
+        The sum of the two integers.
+    """
+    return a + b
